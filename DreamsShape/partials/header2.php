caef60fadb0dc83975326dff10f6c826361fce6f
guiisilva00 <guilherme.p.silva51@aluno.senai.br>
2026-07-24 14:28:56 -0300

    Remoçao das pastas dos projetos antigos para melhor visualização em outros repositórios

--- DreamsShape/partials/header2.php
+++
@@ -1,7 +0,0 @@
-    <header class="header">
-        <div class="container header_title">
-            <a href="index.php" id="title">
-                <h1 class="logo">Dream's <span>Shape</span></h1>
-            </a>
-        </div>
-    </header>
